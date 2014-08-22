db8295ed9aa4ddf71d2dc707e61bbba04ca5b3ef
Brandon Newell <bnewell@myoffice.local>
2014-08-22 14:08:59 -0400

    Added and updated variable names
    +Added TARGET_VMS variable to make it easier to edit for personal use
    +Changed all variables to use the VM_ID SNAP_ID format

--- revert.sh
+++ revert.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-rm nodeid.txt
-vim-cmd vmsvc/getallvms|grep mapr|awk '{printf $1"\n"}' >>nodeid.txt
+TARGET_VMS="mapr"
 
-for vmid in $(cat nodeid.txt);
+rm nodeid.txt
+vim-cmd vmsvc/getallvms|grep $TARGET_VMS|awk '{printf $1"\n"}' >>nodeid.txt
+
+for VM_ID in $(cat nodeid.txt);
     do
-    snapid=$(vim-cmd vmsvc/get.snapshotinfo $vmid|grep id|awk '{print $3}'|sed 's/,//');
-    echo "vim-cmd vmsvc/snapshot.revert $vmid $snapid 0";
-    vim-cmd vmsvc/snapshot.revert $vmid $snapid 0;
-    vim-cmd vmsvc/power.on $vmid;
+    snapid=$(vim-cmd vmsvc/get.snapshotinfo $VM_ID|grep id|awk '{print $3}'|sed 's/,//');
+    echo "vim-cmd vmsvc/snapshot.revert $VM_ID $SNAP_ID 0";
+    vim-cmd vmsvc/snapshot.revert $VM_ID $SNAP_ID 0;
+    vim-cmd vmsvc/power.on $VM_ID;
     done
