f4e75ce28bcbb694c9c13009dda53ef59744a99d
fullaware <bnewell@myoffice.local>
2014-08-22 16:39:58 -0400

    added resnap.sh

---
+++ resnap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+TARGET_VMS="mapr"
+
+rm nodeid.txt
+vim-cmd vmsvc/getallvms|grep $TARGET_VMS|awk '{printf $1"\n"}' >>nodeid.txt
+
+for VM_ID in $(cat nodeid.txt);
+    do
+    echo "vim-cmd vmsvc/power.shutdown $VM_ID";
+    vim-cmd vmsvc/power.shutdown $VM_ID;
+    echo "vim-cmd vmsvc/snapshot.removeall $VM_ID";
+    vim-cmd vmsvc/snapshot.removeall $VM_ID;
+    echo "vim-cmd vmsvc/snapshot.create $VM_ID latest_snapshot snapshot_test 0 0";
+    vim-cmd vmsvc/snapshot.create $VM_ID latest_snapshot snapshot_test 0 0
+    echo "vim-cmd vmsvc/power.on $VM_ID";    
+    vim-cmd vmsvc/power.on $VM_ID;
+    done
