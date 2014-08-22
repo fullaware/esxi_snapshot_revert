af26d3a7b8c8bfb14a9faaee63ed3055fbd2c452
fullaware <fullaware@gmail.com>
2014-08-22 16:40:33 -0400

    Update resnap.sh

--- resnap.sh
+++ resnap.sh
@@ -6,8 +6,6 @@ vim-cmd vmsvc/getallvms|grep $TARGET_VMS|awk '{printf $1"\n"}' >>nodeid.txt
 
 for VM_ID in $(cat nodeid.txt);
     do
-    echo "vim-cmd vmsvc/power.shutdown $VM_ID";
-    vim-cmd vmsvc/power.shutdown $VM_ID;
     echo "vim-cmd vmsvc/snapshot.removeall $VM_ID";
     vim-cmd vmsvc/snapshot.removeall $VM_ID;
     echo "vim-cmd vmsvc/snapshot.create $VM_ID latest_snapshot snapshot_test 0 0";
